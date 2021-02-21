8e033c2de4d7561e3ebea214759729b2d1974011
jeffry delapena <jeffrydelapena@jeffrys-air.lan>
2021-02-20 22:58:35 -0600

    skeletons and sutocode

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+// creat event listners for buttons
+//create functions for search 
+//create button for profile
+//deploy to surge 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#fetch').on('click', run)
+
+// function run (e){
+    
+//     $.get('https://dog.ceo/api/breeds/image/random', (data)=>{
+//    //console.log(data)
+//    var source = data.message
+//    var img = $(`<img class="col" src='${source}' width=20% height=30%></ul>`)
+//    $('#pups').append(img)
+//    counter+=1})
+//     }else{
+       
+// }
+// $('#clear').on('click', e=>{
+//     $('#pups').empty()
+
+// })
